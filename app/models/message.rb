419d15451b462615c1f3bda6c36a59203b16e9e3
Kriszta <matyi.kriszta@gmail.com>
2014-03-06 19:30:40 +0000

    adding fetch_history function, saving result to messages table

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,12 +6,19 @@ class Message < ActiveRecord::Base
 
   API_TOKEN = "4804a586d045024f80e57c088e2295" # TO DO : put in ENV variable
 
-  def fetch_history
+  def self.fetch_history
     client = HipChat::Client.new(API_TOKEN)
     room = client.rooms.select { |room| room.name.include?("WDI London December 2013") }.first
     history_string = room.history()
     history_hash = JSON.parse(history_string)
     messages = history_hash.first[1]
+    messages.each do |message|
+      Message.create(
+        date: message['date'],
+        user_id: User.where(name: message['from']['name']).first.id,
+        body: message['message']
+        )
+    end 
   end
 
 end
