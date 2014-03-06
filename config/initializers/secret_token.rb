8e8ca9b008b7b74260f78e471dcbf66c9be79fea
Laurent Curau <laurentcurau@gmail.com>
2014-03-06 15:42:15 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TalkingHandsApp::Application.config.secret_token = '10ef9216e16e72bb399d53d18e6b19ac3bb355381591139ca012d6671d82d4e3c804f3405694f7430b697d44ee890ae8a3c1afc479ba1d9e9a3c1e0379cea0b8'
