8f1ac8d3c8ca5177ec129eb333b502ac62108a41
Laurent Curau <laurentcurau@gmail.com>
2014-03-07 10:12:33 +0000

    Fetch all messages since beginning of course (te be continued)

--- app/models/message.rb
+++ app/models/message.rb
@@ -12,6 +12,7 @@ class Message < ActiveRecord::Base
     history_string = room.history()
     history_hash = JSON.parse(history_string)
     messages = history_hash.first[1]
+
     messages.each do |message|
 
     User.create name: message['from']['name'], email: "#{message['from']['name'].gsub(/ /, "").downcase}@wdi4.com", password: "password", password_confirmation: "password" unless User.exists?(name: message['from']['name'])
@@ -24,6 +25,33 @@ class Message < ActiveRecord::Base
     end
   end
 
+  def self.fetch_all_messages
+    start_date = Date.parse('2013-12-16').to_s
+    end_date = Date.today.to_s
+
+    client = HipChat::Client.new(API_TOKEN)
+    room = client.rooms.select { |room| room.name.include?("WDI London December 2013") }.first
+
+    start_date.to_s.upto(end_date) do |date|
+      history_string = room.history(date: date, timezone: 'GMT')
+      history_hash = JSON.parse(history_string)
+      messages = history_hash.first[1]
+
+      messages.each do |message|
+
+        User.create name: message['from']['name'], email: "#{message['from']['name'].gsub(/ /, "").downcase}@wdi4.com", password: "password", password_confirmation: "password" unless User.exists?(name: message['from']['name'])
+
+        Message.create(
+          date: message['date'],
+          user_id: User.where(name: message['from']['name']).first.id,
+          body: message['message']
+          )
+      end
+      sleep(5)
+    end
+  end
 end
+
+
 
 
